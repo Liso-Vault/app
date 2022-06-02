36dd11a68d8c8b4b7671050a5f110f37acb9061b
Oliver Martinez <nemoryoliver@gmail.com>
2022-06-02 14:41:17 +0800

    published 0.4.0 for windows

--- lib/core/firebase/auth.service.dart
+++ lib/core/firebase/auth.service.dart
@@ -64,6 +64,7 @@ class AuthService extends GetxService with ConsoleMixin {
   }
 
   Future<void> signOut() async {
+    if (!isFirebaseSupported) return console.warning('Not Supported');
     await instance.signOut();
     console.info('signOut');
   }
