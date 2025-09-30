19dffe90efb4ebc4dd6879d6bf24744c344fd413
nemoryoliver <nemoryoliver@gmail.com>
2025-09-30 12:20:17 +0800

    android updates

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,83 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "com.liso.app"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.liso.app"
+        resValue("string", "app_name", "Liso")
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        multiDexEnabled = true
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = "${keystoreProperties["keyAlias"]}"
+            keyPassword = "${keystoreProperties["keyPassword"]}"
+            storeFile = keystoreProperties["storeFile"]?.let { file("$it") }
+            storePassword = "${keystoreProperties["storePassword"]}"
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    // FLAVORS
+    flavorDimensions += "app"
+    
+    productFlavors {
+        create("production") {
+            dimension = "app"
+        }
+
+        create("development") {
+            dimension = "app"
+            applicationId = "com.liso.app.debug"
+            resValue("string", "app_name", "Liso Debug")
+        }
+    }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+}
+
+flutter {
+    source = "../.."
+}
